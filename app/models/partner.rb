8bc5ffbb0d2f8f87ec476f9f30a484ee37b358a9
zakwan <zakwan.hajjar@startappz.com>
2014-01-30 19:33:07 +0200

    samething i forgat to commit last time

--- app/models/partner.rb
+++ app/models/partner.rb
@@ -4,12 +4,12 @@ class Partner
   include Mongoid::MultiParameterAttributes
 
   field :name, type: String
-  field :governante, type: String
+  field :governante, type: Integer
   field :region, type: String
   field :partnership_start_date, type: Date
   field :status, type: String
   field :osra_id, type: Integer
-  
+
   validates_presence_of :name, :partnership_start_date
-#  validates :partnership_start_date, presence: true 
+#  validates :partnership_start_date, presence: true
 end
